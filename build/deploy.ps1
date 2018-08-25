5ccbac5c849b8a324177331de3805464b9c1fa58
Clint Chapman <cwchapma@gmail.com>
2018-08-25 17:42:00 -0400

    !Deploy

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -3,5 +3,6 @@ if ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match "^!Deploy")
     $ModulePath = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME
     Import-Module PowerShellGet -Force
     Publish-Module -Path $ModulePath -NuGetApiKey ($env:PSGallery_Api_Key) -Confirm:$false
+} else {
+    Write-Output "Not Deployed (waiting for commit message starting with '!Deploy')"
 }
-
