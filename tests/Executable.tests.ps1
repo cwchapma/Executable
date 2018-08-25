6cfb0e63e818815d5450ee7dccf6c33345a400a0
Clint Chapman <cwchapma@gmail.com>
2018-08-25 16:57:31 -0400

    test: add tests

--- tests/Executable.tests.ps1
+++ tests/Executable.tests.ps1
@@ -11,3 +11,38 @@ else
 
 $ModulePath = Join-Path $ProjectPath $ModuleName
 
+dotnet build --configuration Release --runtime win-x64 $PSScriptRoot\TestConsoleApp
+
+Describe 'Invoke-Executable' {
+    $cmd = "$PSScriptRoot\TestConsoleApp\bin\Release\netcoreapp2.0\win-x64\TestConsoleApp.exe"
+
+    It "Returns stdout" {
+        $output = Invoke-Executable $cmd
+        $output | Should -Contain 'Hello from stdout!'
+    }
+
+    It 'Returns stderr as string' {
+        $output = Invoke-Executable $cmd
+        $output | Should -BeOfType String
+        $output | Should -Contain 'Hello from stderr!'
+    }
+
+    It 'Return stderr as ErrorRecord when -StdErrAsErrorRecords' {
+        $output = Invoke-Executable $cmd -StdErrAsErrorRecords
+        $output[1] | Should -BeOfType System.Management.Automation.ErrorRecord
+        $output[1].TargetObject | Should -Be 'Hello from stderr!'
+        $output[1].FullyQualifiedErrorId | Should -Be "NativeCommandError"
+    }
+
+    It 'Passes args properly' {
+        $output = Invoke-Executable "$cmd arg1 arg2"
+        $output | Should -Contain 'Arg: arg1'
+        $output | Should -Contain 'Arg: arg2'
+    }
+
+    It 'Outputs in the right order' {
+        $output = Invoke-Executable "$cmd arg1 arg2"
+        $output | Should -Be "Hello from stdout!", "Hello from stderr!", "Arg: arg1", "Arg: arg2"
+    }
+
+}
---
+++ tests/TestConsoleApp/TestConsoleApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output to stdout because "everybody's doing it"
+            Console.WriteLine("Hello from stdout!");
+
+            // Output to stderr so we can check this doesn't cause a powershell error
+            Console.Error.WriteLine("Hello from stderr!");
+
+            // Output args so we can test they are passed properly
+            foreach (var arg in args) {
+                Console.WriteLine($"Arg: {arg}");
+            }
+        }
+    }
+}
