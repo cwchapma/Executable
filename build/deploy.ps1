0adcbaf7ccc91c1984ef8eae90dd04e4b184911e
Clint Chapman <cwchapma@gmail.com>
2018-08-26 16:39:18 -0400

    build: remove duplicate import from deploy.ps1 (#requires)

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -1,5 +1,4 @@
 #Requires -Modules @{ ModuleName = "PowerShellGet"; ModuleVersion = "1.6.6" }
 
 $ModulePath = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME
-Import-Module PowerShellGet -Force
 Publish-Module -Path $ModulePath -NuGetApiKey ($env:PSGallery_Api_Key) -Confirm:$false
