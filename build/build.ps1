24f00e237efa027ec9f9d9ca4be712ff0d35f857
Clint Chapman <cwchapma@gmail.com>
2018-08-26 19:10:01 -0400

    test: only publish test results if appveyor build

--- build/build.ps1
+++ build/build.ps1
@@ -42,7 +42,9 @@ Invoke-Pester `
     -OutputFormat NUnitXml `
     -OutputFile $testResultsFile
 
-(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
-if ($res.FailedCount -gt 0) { 
-    throw "$($res.FailedCount) tests failed."
+if ($env:APPVEYOR) {
+    (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
+    if ($res.FailedCount -gt 0) { 
+        throw "$($res.FailedCount) tests failed."
+    }
 }
