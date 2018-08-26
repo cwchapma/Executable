7642088aee1ac6b42584298be10f7edf7c4453f2
Clint Chapman <cwchapma@gmail.com>
2018-08-26 18:44:46 -0400

    fix: Import-PackageProvider to support prereleases

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -1,4 +1,7 @@
 #Requires -Modules @{ ModuleName = "PowerShellGet"; ModuleVersion = "1.6.6" }
 
+Import-Module PowerShellGet -Force
+Import-PackageProvider PowerShellGet -Force
+
 $ModulePath = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME
 Publish-Module -Path $ModulePath -NuGetApiKey ($env:PSGallery_Api_Key) -Confirm:$false
