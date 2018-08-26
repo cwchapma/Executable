2a68a9e3eae77b46cd36f7bafda49f0cbef5ad87
Clint Chapman <cwchapma@gmail.com>
2018-08-26 16:34:35 -0400

    build: update deploy.ps1 with requires

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -1,3 +1,5 @@
+#Requires -Modules @{ ModuleName = "PowerShellGet"; ModuleVersion = "1.6.6" }
+
 $ModulePath = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME
 Import-Module PowerShellGet -Force
 Publish-Module -Path $ModulePath -NuGetApiKey ($env:PSGallery_Api_Key) -Confirm:$false
