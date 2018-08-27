9a8088a174f13a40c0ff78c0f1f4203c5d0b5f73
Clint Chapman <cwchapma@gmail.com>
2018-08-26 20:05:38 -0400

    build: Set minimum powershell package provider version to 1.6.6

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -1,7 +1,7 @@
 #Requires -Modules @{ ModuleName = "PowerShellGet"; ModuleVersion = "1.6.6" }
 
 Import-Module PowerShellGet -Force
-Import-PackageProvider PowerShellGet -Force
+Import-PackageProvider PowerShellGet -MinimumVersion "1.6.6" -Force
 
 $ModulePath = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME
 Publish-Module -Path $ModulePath -NuGetApiKey ($env:PSGallery_Api_Key) -Confirm:$false
