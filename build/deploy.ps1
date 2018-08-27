c2ee3037e8260a60d24b4a288c27bcb43124688c
Clint Chapman <cwchapma@gmail.com>
2018-08-26 20:56:57 -0400

    test: remove extra import

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -1,6 +1,5 @@
 #Requires -Modules @{ ModuleName = "PowerShellGet"; ModuleVersion = "1.6.6" }
 
-Import-Module PowerShellGet -Force
 Import-PackageProvider PowerShellGet -MinimumVersion "1.6.6" -Force
 
 $ModulePath = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME
